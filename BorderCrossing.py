7ba5ca4db1239c52251ecf66164c4830ea6bd180
Krzysiek <krzysiek.ziach@gmail.com>
2020-05-13 17:35:51 +0200

    Added forecasting with LSTM

--- BorderCrossing.py
+++ BorderCrossing.py
@@ -5,13 +5,17 @@
 # standard imports 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 from datetime import datetime
 import calendar
 import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from sklearn.metrics import mean_squared_error
 # forecasting imports
 import tensorflow as tf
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.models import Model
 
 # laod the dataset
 dataset = pd.read_csv('data/Border_Crossing_Entry_Data.csv')
@@ -192,4 +196,129 @@ plt.show()
 
 """ Forecasting for 2020 using Keras' LSTM """
 
-regression_data = dataset[['Value', 'Month', 'Year']].groupby('Month')['Value'].sum()
+data = dataset[['Value', 'Month', 'Year']]
+years = list(data['Year'].value_counts().index)
+years.sort()
+
+feed_data = pd.DataFrame()
+for year in years:
+    data_each_year = data[data['Year'] == year]
+    month_in_year = pd.DataFrame(data_each_year.groupby('Month')['Value'].sum())
+    month_in_year['Month'] = month_in_year.index
+    month_in_year['Year'] = year
+    feed_data = pd.concat([feed_data, month_in_year], ignore_index=True)
+
+# feed_data_encoded = pd.get_dummies(feed_data, ['Month'])
+
+# encode months
+feed_data_encoded = feed_data.copy()
+label_encoder = LabelEncoder()
+feed_data_encoded['Month'] = label_encoder.fit_transform(feed_data_encoded['Month'])
+# increment so the numbers correlate to actual months
+feed_data_encoded.loc[:, 'Month'] +=1
+
+# scale
+scaler = MinMaxScaler()
+feed_data_scaled = scaler.fit_transform(feed_data_encoded)
+
+# LSTM expects 3 inputs
+# samples 
+# time steps - one time step is one observation (so Value?)
+# features - month and  year?
+# all in all, (sample, time_step, (feature1, feature2, ...))
+
+# split into train and test sets
+n_years = 14
+n_months = n_years * 12
+# first 18 years
+train = feed_data_scaled[:n_months, :]
+# what's remaining 
+test = feed_data_scaled[n_months:, :] # ignoring 2 months from 2020
+
+# split into input and outputs
+train_X, train_y = train[:, 1:], train[:, 0]
+test_X, test_y = test[:, 1:], test[:, 0]
+
+# reshape input to be 3D [samples, timesteps, features]
+train_X = train_X.reshape(train_X.shape[0], 1, train_X.shape[1])
+test_X = test_X.reshape(test_X.shape[0], 1, test_X.shape[1])
+print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+
+# neural network using LSTM
+model = Sequential()
+model.add(LSTM(20, input_shape=(train_X.shape[1], train_X.shape[2]), dropout=0.1, go_backwards=True))
+model.add(Dense(1))
+
+model.compile(optimizer='adam',
+              loss='mae')
+
+r = model.fit(train_X, train_y, epochs=100, validation_data=(test_X, test_y))
+
+# plot history
+plt.plot(r.history['loss'], label='loss')
+plt.plot(r.history['val_loss'], label='val_loss')
+plt.legend()
+plt.show()
+
+# make a prediction
+yhat = model.predict(test_X)
+test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
+
+# invert scaling for forecast
+inv_yhat = np.concatenate((yhat, test_X), axis=1)
+inv_yhat = scaler.inverse_transform(inv_yhat)
+inv_yhat = inv_yhat[:, 0]
+
+# invert scaling for actual
+test_y = test_y.reshape((len(test_y), 1))
+inv_y = np.concatenate((test_y, test_X), axis=1)
+inv_y = scaler.inverse_transform(inv_y)
+inv_y = inv_y[:,0]
+
+# calculate RMSE
+rmse = np.sqrt(mean_squared_error(inv_y, inv_yhat))
+print('Test RMSE: %.3f' % rmse)
+
+# months in the order of train data
+# that is Jan 2010 - Dec 2020 and extra Jan and Feb for 2020
+month_idx_start = np.arange(1, 13)
+month_idx_start = np.tile(month_idx_start, 10).reshape(-1, 1)
+month_idx_end = np.array([[1], [2]])
+full_months = np.concatenate((month_idx_start, month_idx_end))
+
+# prepare inverted y data
+inv_y_df = pd.DataFrame(inv_y, columns=['Value'])
+inv_y_df['Month'] = full_months
+inv_y_df['Month'] = inv_y_df['Month'].astype(int)
+inv_y_df['Month'] = inv_y_df['Month'].apply(lambda x: calendar.month_abbr[x])
+inv_y_df['Type'] = 'Actual'
+
+# prepare inverted yhat data
+inv_yhat_df = pd.DataFrame(inv_yhat, columns=['Value'])
+inv_yhat_df['Month'] = full_months
+inv_yhat_df['Month'] = inv_yhat_df['Month'].astype(int)
+inv_yhat_df['Month'] = inv_yhat_df['Month'].apply(lambda x: calendar.month_abbr[x])
+inv_yhat_df['Type'] = 'Predicted'
+
+full_results_df = pd.concat([inv_y_df, inv_yhat_df])
+
+# append years, that is 2010 - 2019 (12 times each) + 2 times 2020
+years = np.array([2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019])
+years = np.repeat(years, 12)
+last_years = np.array([2020, 2020])
+years = np.append(years, last_years)
+years = np.tile(years, 2)
+
+full_results_df['Year'] = years
+
+full_results_df['Date'] = full_results_df['Month'] + full_results_df['Year'].astype(str)
+ordered_dates = full_results_df['Date'].unique()
+full_results_df['Date'] = pd.Categorical(full_results_df['Date'], categories=ordered_dates,
+               ordered=True)
+
+plt.figure(figsize=(25, 8))
+sns.lineplot(x='Date', y='Value', hue='Type', data=full_results_df)
+plt.legend()
+plt.grid(True)
+plt.xticks(rotation=90)
+plt.show()
